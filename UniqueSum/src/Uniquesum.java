dd4a9f51cfa94b92978f445301ebaccdf47c4a4e
kcostawelch <147612420+kcostawelch@users.noreply.github.com>
2023-10-20 11:31:17 +0100

    Add files via upload
    Updates to include error catching for non-integer inputs.

--- UniqueSum/src/Uniquesum.java
+++ UniqueSum/src/Uniquesum.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 public class Uniquesum {
     public static void main(String[] args) {
         Scanner scanner =  new Scanner(System.in);
-
-        System.out.println("Input an integer:");
-        int num1 = scanner.nextInt();
-
-        System.out.println("Input a second integer:");
-        int num2 = scanner.nextInt();
-
-        System.out.println("Input a third integer:");
-        int num3 = scanner.nextInt();
+        int num1 = 0, num2 = 0 , num3 = 0;
+        boolean numTrue = false;
+        while (!numTrue) {
+            try {
+                System.out.println("Input an integer:");
+                num1 = scanner.nextInt();
+                System.out.println("Input a second integer:");
+                num2 = scanner.nextInt();
+                System.out.println("Input a third integer:");
+                num3 = scanner.nextInt();
+                numTrue = true;
+            }
+            catch (Exception e) {
+                System.out.println("You must enter an integer.");
+                scanner.nextLine();
+            }
+        }
 
         if (num1 == num2 && num1 == num3) {
             num1 = num2 = num3 =0;
